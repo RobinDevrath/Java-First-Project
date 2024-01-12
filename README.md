16b073d39d54030e9c7483726a15d0a9f31a19ce
RobinDevrath <robinkumar3498@gmail.com>
2024-01-13 00:24:43 +0530

    Addition of two numbers

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Java-First-Project
 First Project 
+<br>
+my name is robin
---
+++ first.java
@@ -0,0 +1,16 @@
+public class first { public static void main(String[] args) {
+    
+
+
+int a = 10;
+int b =5 ;
+System.out.println(a + b);
+
+
+
+
+
+}
+
+    
+}
